d8ec1d1e41480c8f698d4856dabf748a7fa1a4b3
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-04 21:10:47 +0530

    Update HomeController.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/HomeController.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/HomeController.java
@@ -1,23 +1,37 @@
 package com.example.SpringBootLearning.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringBootLearning.service.HomeService;
 
 @RestController
 public class HomeController {
 	
+	private final HomeService homeService;
+	
+	public HomeController(HomeService homeService) {
+		this.homeService = homeService;
+	}
+	
 	@GetMapping("/")
 	public String home() {
-		return "Welcome to Spring Boot!";
+		return homeService.getHomeMessage();
 	}
 	
 	@GetMapping("/about")
 	public String about() {
-		return "This is my first Spring Boot project .";
+		return homeService.getAboutMessage();
 	}
 	
 	@GetMapping("/name")
 	public String name() {
-		return "Chetan Aavarkar";
+		return homeService.getNameMessage();
+	}
+	
+	@GetMapping("/hello-user/{name}")
+	public String hello(@PathVariable String name) {
+		return homeService.getHelloMessage(name);
 	}
 }
