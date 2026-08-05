bba489fdc2f91662fe0747616b97b88ccbe6fb1c
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-05 22:04:50 +0530

    Update StudentController.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
@@ -1,14 +1,28 @@
 package com.example.SpringBootLearning.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringBootLearning.model.Student;
 
 @RestController
 public class StudentController {
 	
 	@GetMapping("/student")
-	public String getStudentInfp(@RequestParam int id, @RequestParam String name) {
-		return "Student ID: " + id + "\nStudent Name: " + name;
+	public Student getStudent() {
+		return new Student(101, "Chetan", "Surat");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		ArrayList<Student> students = new ArrayList<>();
+		students.add(new Student(101, "Chetan", "Surat"));
+		students.add(new Student(102, "Rahul", "Ahmedabad"));
+		students.add(new Student(103, "Amit", "Vadodara"));
+		return students;
 	}
 }
