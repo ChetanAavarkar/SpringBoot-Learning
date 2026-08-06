91c831ab6b68a6def62be0b4a06479b1e2633726
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-06 20:35:38 +0530

    Update StudentController.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
@@ -4,25 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringBootLearning.model.Student;
+import com.example.SpringBootLearning.service.HomeService;
 
 @RestController
 public class StudentController {
 	
-	@GetMapping("/student")
-	public Student getStudent() {
-		return new Student(101, "Chetan", "Surat");
+	private final HomeService homeService;
+	
+	public StudentController(HomeService homeService) {
+		this.homeService = homeService;
 	}
 	
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		ArrayList<Student> students = new ArrayList<>();
-		students.add(new Student(101, "Chetan", "Surat"));
-		students.add(new Student(102, "Rahul", "Ahmedabad"));
-		students.add(new Student(103, "Amit", "Vadodara"));
-		return students;
+	@GetMapping("/student/{id}")
+	public Student getStudentById(@PathVariable int id) {
+		return homeService.getStudentById(id);
+	}
+	
+	@GetMapping("/students/city/{city}")
+	public List<Student> getStudentsByCity(@PathVariable String city) {
+		return homeService.getStudentsByCity(city);
 	}
 }
