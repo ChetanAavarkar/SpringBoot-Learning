5b3f6abe9bfa1698c7be6ccd83d5ce4eb1a84e66
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-13 19:05:16 +0530

    Update StudentController.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,10 @@ public class StudentController {
 	public ResponseEntity<String> deleteStudent(@PathVariable int id) {
 		homeService.deleteStudent(id);
 		return ResponseEntity.ok("Student with ID " + id + " deleted successfully");
+	}
+	
+	@PutMapping("/{id}")
+	public Student updateStudent(@PathVariable int id, @RequestBody Student updatedStudent) {
+	    return homeService.updateStudent(id, updatedStudent.getName(), updatedStudent.getCity());
 	}
 }
