6a9b0aca03a14b0722c84eb1c2b653b68e76a128
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-18 18:17:16 +0530

    Update StudentController.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringBootLearning.dto.StudentDTO;
+import com.example.SpringBootLearning.dto.StudentRequestDTO;
+import com.example.SpringBootLearning.dto.StudentResponseDTO;
 import com.example.SpringBootLearning.model.Student;
 import com.example.SpringBootLearning.service.HomeService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/students")
@@ -57,5 +62,12 @@ public class StudentController {
 	@PutMapping("/{id}")
 	public Student updateStudent(@PathVariable int id, @RequestBody Student updatedStudent) {
 	    return homeService.updateStudent(id, updatedStudent.getName(), updatedStudent.getCity());
+	}
+	
+	@PostMapping("/students")
+	public ResponseEntity<StudentResponseDTO> createStudent(@Valid @RequestBody StudentRequestDTO requestDTO) {
+		
+		StudentResponseDTO response = homeService.createStudent(requestDTO);
+		return ResponseEntity.ok(response);
 	}
 }
