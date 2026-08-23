22cc0c439b39d41432cd0d14d0f8b5e8797d3905
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-23 18:46:02 +0530

    Update StudentRepository.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/repository/StudentRepository.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/repository/StudentRepository.java
@@ -1,8 +1,13 @@
 package com.example.SpringBootLearning.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.SpringBootLearning.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+	List<Student> findByCity(String city);
+	List<Student> findByName(String name);
+	List<Student> findByNameContaining(String name);
+	List<Student> findByCityIgnoreCase(String city);
 }
