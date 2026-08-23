3c29282a0a35261bbdcf83316b2053da603e443a
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-23 11:38:41 +0530

    Update SpringBootLearningApplication.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/SpringBootLearningApplication.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/SpringBootLearningApplication.java
@@ -1,13 +1,22 @@
 package com.example.SpringBootLearning;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLearningApplication {
+public class SpringBootLearningApplication implements CommandLineRunner {
+	
+	@Autowired
+	private AppInfo appInfo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 	}
 
+	@Override
+	public void run(String...args) {
+		appInfo.printAppName();
+	}
 }
