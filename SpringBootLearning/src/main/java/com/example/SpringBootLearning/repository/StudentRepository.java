5661b013b49b5087ce2e26e16baa3d57681bba19
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-25 19:03:54 +0530

    Update StudentRepository.java

--- SpringBootLearning/src/main/java/com/example/SpringBootLearning/repository/StudentRepository.java
+++ SpringBootLearning/src/main/java/com/example/SpringBootLearning/repository/StudentRepository.java
@@ -1,7 +1,13 @@
 package com.example.SpringBootLearning.repository;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.SpringBootLearning.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
@@ -11,4 +17,5 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
 	List<Student> findByName(String name);
 	List<Student> findByNameContaining(String name);
 	List<Student> findByCityIgnoreCase(String city);
+	Page<Student> findAll(Pageable pageable);
 }
